f1d81fdd5bb5aaad6e6f3940b345a1486d6221f1
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-08-07 13:40:21 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 """
-City Class from Models Module
+Defines city
 """
-import os
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
-    """City class handles all application cities"""
-    if STORAGE_TYPE == "db":
-        __tablename__ = 'cities'
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities', cascade='delete')
-    else:
-        state_id = ''
-        name = ''
-        places = []
+class City(BaseModel):
+    """defines city to look for"""
+    state_id = ""
+    name = ""
