6f85046cedc8362b3e88e89e057ef09633770a44
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-08-07 14:18:22 +0100

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -35,7 +35,6 @@ class TestCity(unittest.TestCase):
         self.assertIsInstance(self.c.id, str)
         self.assertIsInstance(self.c.created_at, datetime.datetime)
         self.assertIsInstance(self.c.updated_at, datetime.datetime)
-
-        
+       
 if __name__ == '__main__':
     unittest.main()
