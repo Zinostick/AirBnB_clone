ea497734d9788b2aedc63c07e7c9a7825b79b021
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-08-07 13:37:53 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
 """
-Amenity Class from Models Module
+Defines amenities
 """
-import os
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.orm import backref
-STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """Amenity class handles all application amenities"""
-    if STORAGE_TYPE == "db":
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('PlaceAmenity',
-                                       backref='amenities',
-                                       cascade='delete')
-    else:
-        name = ''
+class Amenity(BaseModel):
+    """Defines amenities that user can choose from to offer at its place"""
+    name = ""
